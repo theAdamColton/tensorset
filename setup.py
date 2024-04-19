2748c2f4c8e6ed64284d4b053a2c0749e5d85777
adamcolton <atcolton@tutanota.com>
2024-04-19 16:45:54 -0600

    0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tensorsequence",
-    version="0.0.1",
+    version="0.0.2",
     description="manipulate sequences of tensors",
     author="Adam Colton",
     url="https://github.com/theAdamColton/tensorsequence",
